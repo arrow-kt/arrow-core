a2c7818d6336338d59290b92c595582e30af82db
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-05-08 14:17:04 +0200

    Add missing import

--- arrow-core-data/src/main/kotlin/arrow/typeclasses/Traverse.kt
+++ arrow-core-data/src/main/kotlin/arrow/typeclasses/Traverse.kt
@@ -352,6 +352,7 @@ import arrow.core.ValidatedNel
  * import arrow.fx.IO
  * import arrow.fx.extensions.fx
  * import arrow.fx.extensions.io.concurrent.parTraverse
+ * import arrow.fx.unsafeRunSync
  *
  * interface Profile
  * interface User
