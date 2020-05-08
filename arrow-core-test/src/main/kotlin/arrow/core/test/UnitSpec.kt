2d5daee4663641146623f4d84d5dcf985ddfc140
Pablisco <pabs87@gmail.com>
2020-05-09 00:00:48 +0100

    Make test cases even more lazy

--- arrow-core-test/src/main/kotlin/arrow/core/test/UnitSpec.kt
+++ arrow-core-test/src/main/kotlin/arrow/core/test/UnitSpec.kt
@@ -2,6 +2,7 @@ package arrow.core.test
 
 import arrow.core.test.laws.Law
 import io.kotlintest.TestCase
+import io.kotlintest.TestContext
 import io.kotlintest.TestType
 import io.kotlintest.specs.AbstractStringSpec
 
@@ -10,8 +11,8 @@ import io.kotlintest.specs.AbstractStringSpec
  */
 abstract class UnitSpec(
   private val laws: List<Law> = emptyList(),
-  tests: AbstractStringSpec.() -> Unit = {}
-) : AbstractStringSpec(tests) {
+  private val tests: UnitSpecContext.() -> Unit = {}
+) : AbstractStringSpec() {
 
   private val deprecatedLawTestCases = mutableListOf<TestCase>()
 
@@ -25,12 +26,29 @@ abstract class UnitSpec(
       lawTestCase
     }
 
-  override fun testCases(): List<TestCase> = super.testCases() + deprecatedLawTestCases + lawsTestCases
+  override fun testCases(): List<TestCase> =
+    (super.testCases() + deprecatedLawTestCases + lawsTestCases + testCases).distinctBy { it.name }
 
   private val lawsTestCases: List<TestCase> by lazy {
-    laws.distinctBy { law: Law -> law.name }
-      .map { law: Law -> createTestCase(law.name, law.test, defaultTestCaseConfig, TestType.Test) }
+    laws.map { law: Law -> createTestCase(law.name, law.test) }
   }
+
+  private val testCases: List<TestCase> by lazy {
+    UnitSpecContext().apply(tests).tests.map { (name, test) -> createTestCase(name, test) }
+  }
+
+  private fun createTestCase(name: String, test: suspend TestContext.() -> Unit) =
+    createTestCase(name, test, defaultTestCaseConfig, TestType.Test)
 }
 
 fun listOfLaws(vararg laws: List<Law>): List<Law> = laws.toList().flatten()
+
+class UnitSpecContext {
+
+  private val _tests = mutableMapOf<String, suspend TestContext.() -> Unit>()
+  internal val tests: Map<String, suspend TestContext.() -> Unit> = _tests
+
+  operator fun String.invoke(test: suspend TestContext.() -> Unit) {
+    _tests[this] = test
+  }
+}
