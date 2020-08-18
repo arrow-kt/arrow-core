f4065f8f66b9ba1d720e4da5ee66363c9587f2ef
Raul Raja <raulraja@gmail.com>
2020-08-18 02:46:40 +0200

    support for Trans1

--- arrow-continuations/src/test/kotlin/trans/MonadSuspend.kt
+++ arrow-continuations/src/test/kotlin/trans/MonadSuspend.kt
@@ -17,17 +17,27 @@ abstract class MonadSuspend<F> {
   abstract suspend fun <B> B.just(): Kind<F, B>
   operator fun <G> div(other: MonadSuspend<G>): Trans2<F, G> =
     Trans2(this, other)
-  suspend operator fun <A> div(f: suspend MonadSuspend<F>.() -> A): A =
-    f(this)
+  suspend operator fun <Z : Kind<F, A>, A> div(f: suspend Trans<F>.() -> A): Z =
+    f(Trans(this)).just() as Z
+}
+
+open class Trans<F>(open val MF: MonadSuspend<F>) {
+
+  suspend operator fun <B> Kind<F, B>.invoke(): B =
+    MF.run { this@invoke() }
+
+  suspend fun <B> B.just(): Kind<F, B> =
+    MF.run { this@just.just() }
+
+  suspend operator fun <Z : Kind<F, A>, A> div(f: suspend Trans<F>.() -> A): Z =
+    f(this).just() as Z
+
 }
 
 open class Trans2<F, G>(
-  val MF: MonadSuspend<F>,
+  override val MF: MonadSuspend<F>,
   val MG: MonadSuspend<G>
-) {
-  @JvmName("invokeF")
-  suspend operator fun <B> Kind<F, B>.invoke(): B =
-    MF.run { this@invoke() }
+) : Trans<F>(MF) {
 
   @JvmName("invokeG")
   suspend operator fun <B> Kind<G, B>.invoke(): B =
@@ -37,20 +47,23 @@ open class Trans2<F, G>(
   suspend operator fun <B> Kind<F, Kind<G, B>>.invoke(): B =
     MF.run { MG.run { this@invoke() }() }
 
-  suspend fun <B> B.just(): Kind<F, Kind<G, B>> =
+  @JvmName("justFG")
+  suspend fun <B> B.just(unit: Unit = Unit): Kind<F, Kind<G, B>> =
     MF.run { MG.run { this@just.just() }.just() }
 
   operator fun <H> div(other: MonadSuspend<H>): Trans3<F, G, H> =
     Trans3(this, other)
 
+  @JvmName("run2")
   suspend operator fun <Z : Kind<F, Kind<G, A>>, A> div(f: suspend Trans2<F, G>.() -> A): Z =
     f(this).just() as Z
 }
 
+
 class Trans3<F, G, H>(
   val trans2: Trans2<F, G>,
   val MH: MonadSuspend<H>
-): Trans2<F, G>(trans2.MF, trans2.MG) {
+) : Trans2<F, G>(trans2.MF, trans2.MG) {
 
   @JvmName("invokeH")
   suspend operator fun <B> Kind<H, B>.invoke(): B =
@@ -60,6 +73,11 @@ class Trans3<F, G, H>(
   suspend operator fun <B> Kind<F, Kind<G, Kind<H, B>>>.invoke(): B =
     trans2.run { MH.run { this@invoke() }() }
 
+  @JvmName("justFGH")
+  suspend fun <B> B.just(unit: Unit = Unit, unit2: Unit = Unit): Kind<F, Kind<G, Kind<H, B>>> =
+    MF.run {
+      MG.run { MH.run { this@just.just() }.just() }.just()
+    }
 }
 
 fun <A> DelimitedScope<A>.EvalT(): MonadSuspend<ForEval> =
@@ -74,6 +92,11 @@ fun <A> DelimitedScope<A>.OptionT(): MonadSuspend<ForOption> =
       fix().fold({ shift { TODO() } }, ::identity)
 
     override suspend fun <B> B.just(): Kind<ForOption, B> = Some(this)
+  }
+
+suspend fun <A> eval(f: suspend Trans<ForEval>.() -> A): Eval<A> =
+  DelimContScope.reset {
+    EvalT() / f
   }
 
 suspend fun <A> evalOption(f: suspend Trans2<ForEval, ForOption>.() -> A): Eval<Option<A>> =
