6ed1931d24023a75a991f6da27196c2f6e91c2e1
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-10-07 15:55:49 +0200

    Add test

---
+++ arrow-prelude/src/test/kotlin/arrow/ExtensionTest.kt
@@ -0,0 +1,35 @@
+package arrow
+
+import arrow.meta.plugin.testing.CompilerPlugin
+import arrow.meta.plugin.testing.CompilerTest
+import arrow.meta.plugin.testing.Dependency
+import arrow.meta.plugin.testing.assertThis
+import org.junit.jupiter.api.Test
+
+class ExtensionTest {
+
+  @Test
+  fun `extension function`() {
+    val arrowMetaVersion = System.getProperty("ARROW_META_VERSION")
+    val arrowMetaCompilerPlugin = CompilerPlugin("Arrow Meta", listOf(Dependency("compiler-plugin:$arrowMetaVersion")))
+    val prelude = Dependency("arrow-prelude")
+
+
+    assertThis(CompilerTest(
+      config = {
+        addCompilerPlugins(arrowMetaCompilerPlugin) + CompilerTest.addDependencies(prelude)
+      },
+      code = {
+        """
+        |  import arrow.*
+        |  import arrowx.*       
+        |  
+        | val result = "one-".combine("two")
+        """.source
+      },
+      assert = {
+        allOf("result".source.evalsTo("one-two"))
+      }
+    ))
+  }
+}
