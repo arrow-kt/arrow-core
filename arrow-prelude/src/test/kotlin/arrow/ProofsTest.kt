eff1cafe4b88e3c8650ad3d52021797b7a96bf22
danimontoya <daniel.montoya@47deg.com>
2020-10-07 17:03:57 +0200

    Add test for coercion

--- arrow-prelude/src/test/kotlin/arrow/ProofsTest.kt
+++ arrow-prelude/src/test/kotlin/arrow/ProofsTest.kt
@@ -46,14 +46,35 @@ class ProofsTest {
       },
       code = {
         """
-        |  import arrow.*
-        |  import arrowx.*       
+        | import arrow.*
+        | import arrowx.*      
         |  
-        | val result = User.load(Id("Curry"))
+        | inline class Id(val value: String)
+        |  
+        | data class User(val id: Id, val name: String) { // User does not need to directly extend Repository<User>
+        |   companion object
+        | }
+        | 
+        | fun interface Repository<A> {
+        |     fun load(id: Id): A
+        |     interface Syntax<A> {
+        |       val value: A
+        |       suspend fun save(): Unit
+        |    }
+        | }
+        |  object UserRepository : Repository<User> {
+        |     override fun load(id: Id): User = User(id, "Curry")
+        | }
+        | 
+        | @arrow.Coercion
+        | fun User.Companion.repository(): Repository<User> =
+        |   UserRepository
+        |
+        | val result = User.load(Id("Curry")).name
         """.source
       },
       assert = {
-        allOf("result".source.evalsTo(User(Id("Curry"), "Curry")))
+        allOf("result".source.evalsTo("Curry"))
       }
     ))
   }
