473faf81db6aa1d81d65079c72cab18546b8edd2
danimontoya <daniel.montoya@47deg.com>
2020-10-07 16:15:39 +0200

    Enable back Semigroup

--- arrow-prelude/src/main/kotlin/arrow/typeclasses.kt
+++ arrow-prelude/src/main/kotlin/arrow/typeclasses.kt
@@ -1,31 +1,25 @@
-//package arrowx
-//
-//import kotlin.properties.ReadOnlyProperty
-//import kotlin.reflect.KProperty
-//
-//fun interface Semigroup<A> {
-//  fun A.combine(other: A): A
-//  interface Syntax<A> {
-//    val value: A
-//    fun combine(other: A): A
-//  }
-//}
-//operator fun String.getValue(nothing: Nothing?, property: KProperty<*>): Semigroup.Syntax<String> =
-//  StringSyntax(this)
-//
-//inline class StringSyntax(override val value: String) : Semigroup.Syntax<String>, ReadOnlyProperty<String, Semigroup.Syntax<String>> {
-//  override fun combine(other: String): String =
-//    value + other
-//
-//  override fun getValue(thisRef: String, property: KProperty<*>): Semigroup.Syntax<String> = this
-//}
-//
-//@arrow.Extension
-//fun String.semigroupSyntax(): Semigroup.Syntax<String> =
-//  StringSyntax(this)
-//
-//fun main() {
-////  val x: Semigroup.Syntax<String> = "imran"
-//
-//  println("one2-".combine("other"))
-//}
+package arrowx
+
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+fun interface Semigroup<A> {
+  fun A.combine(other: A): A
+  interface Syntax<A> {
+    val value: A
+    fun combine(other: A): A
+  }
+}
+operator fun String.getValue(nothing: Nothing?, property: KProperty<*>): Semigroup.Syntax<String> =
+  StringSyntax(this)
+
+inline class StringSyntax(override val value: String) : Semigroup.Syntax<String>, ReadOnlyProperty<String, Semigroup.Syntax<String>> {
+  override fun combine(other: String): String =
+    value + other
+
+  override fun getValue(thisRef: String, property: KProperty<*>): Semigroup.Syntax<String> = this
+}
+
+@arrow.Extension
+fun String.semigroupSyntax(): Semigroup.Syntax<String> =
+  StringSyntax(this)
