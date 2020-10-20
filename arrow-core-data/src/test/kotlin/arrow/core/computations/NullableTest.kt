807510962c4b411697368b7c8fd6d0f11f9f9009
kioba <charlssom@gmail.com>
2020-10-20 14:24:00 +0100

    Provide extra test cases for nullable.kt

--- arrow-core-data/src/test/kotlin/arrow/core/computations/NullableTest.kt
+++ arrow-core-data/src/test/kotlin/arrow/core/computations/NullableTest.kt
@@ -6,18 +6,55 @@ import io.kotlintest.shouldBe
 class NullableTest : UnitSpec() {
 
   init {
+    "simple case" {
+      nullable {
+        "s".length().invoke()
+      } shouldBe 1
+    }
     "multiple types" {
       nullable {
-          val number = "s".length.bind()
-          val string = number.toString().bind()
-          string
+        val number = "s".length()
+        val string = number.toString()()
+        string
       } shouldBe "1"
     }
     "short circuit" {
-      nullable<String> {
-          val number: Int = "s".length.bind()
-          number.takeIf { it > 1 }?.toString().bind()
-          throw IllegalStateException("This should not be executed")
+      nullable {
+        val number: Int = "s".length()
+        (number.takeIf { it > 1 }?.toString())()
+        throw IllegalStateException("This should not be executed")
+      } shouldBe null
+    }
+    "when expression" {
+      nullable {
+        val number = "s".length()
+        val string = when (number) {
+          1 -> number.toString()
+          else -> null
+        }.invoke()
+        string
+      } shouldBe "1"
+    }
+    "if expression" {
+      nullable {
+        val number = "s".length()
+        val string = if (number == 1) {
+          number.toString()
+        } else {
+          null
+        }.invoke()
+        string
+      } shouldBe "1"
+    }
+    "if expression short circuit" {
+      nullable {
+        val number = "s".length()
+        val string = if (number != 1) {
+          number.toString()
+        } else {
+          null
+        }()
+        string
       } shouldBe null
     }
   }
