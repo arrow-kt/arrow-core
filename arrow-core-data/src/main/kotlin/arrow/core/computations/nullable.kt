78bb5f6e9d224995d174b78abf0f2f2bd696c52a
Simon Vergauwen <vergauwen.simon@gmail.com>
2020-11-24 21:37:59 +0100

    Re-add eager versions, fix `RestrictSuspension` in `nullable` and add suspend version

--- arrow-core-data/src/main/kotlin/arrow/core/computations/nullable.kt
+++ arrow-core-data/src/main/kotlin/arrow/core/computations/nullable.kt
@@ -1,12 +1,25 @@
 package arrow.core.computations
 
+import arrow.continuations.generic.DelimContScope
 import arrow.continuations.generic.DelimitedScope
 import arrow.core.computations.suspended.BindSyntax
+import arrow.core.computations.suspended.EagerBindSyntax
 
 @Suppress("ClassName")
 object nullable {
-  operator fun <A> invoke(func: suspend BindSyntax.() -> A?): A? = TODO()
-//    DelimContScope.reset { NullableBindSyntax(this).func() }
+  fun <A> eager(func: suspend EagerBindSyntax.() -> A?): A? = runRestrictedSuspension {
+    DelimContScope.reset { func(NullableEagerBindSyntax(this)) }
+  }
+
+  suspend operator fun <A> invoke(func: suspend BindSyntax.() -> A?): A? =
+    DelimContScope.reset { func(NullableBindSyntax(this)) }
+
+  private class NullableEagerBindSyntax<R>(
+    scope: DelimitedScope<R?>
+  ) : EagerBindSyntax, DelimitedScope<R?> by scope {
+    override suspend fun <A> A?.invoke(): A =
+      this ?: shift { null }
+  }
 
   private class NullableBindSyntax<R>(
     scope: DelimitedScope<R?>
