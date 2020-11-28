60391354ac0030bda784aed6e9097edfc55bda30
Raul Raja <raulraja@gmail.com>
2020-11-28 19:16:27 +0100

    Effect interface and computation builder

---
+++ arrow-continuations/src/main/kotlin/arrow/continuations/Effect.kt
@@ -0,0 +1,14 @@
+package arrow.continuations
+
+import arrow.continuations.generic.DelimitedScope
+
+fun interface Effect<F> {
+  fun control(): DelimitedScope<F>
+}
+
+suspend inline fun <FA, Eff: Effect<FA>, A> computation(
+  crossinline just : (A) -> FA,
+  crossinline eff: (DelimitedScope<FA>) -> Eff,
+  crossinline block: suspend Eff.() -> A
+): FA =
+  Reset.single { just(block(eff(this))) }
