6860fff8199281b2b454bc695963feba91168d91
Simon Vergauwen <vergauwen.simon@gmail.com>
2020-12-21 11:41:28 +0100

    Alternative

--- arrow-core/src/main/kotlin/arrow/core/extensions/list/alternative/ListKAlternative.kt
+++ arrow-core/src/main/kotlin/arrow/core/extensions/list/alternative/ListKAlternative.kt
@@ -2,9 +2,12 @@ package arrow.core.extensions.list.alternative
 
 import arrow.Kind
 import arrow.core.ForListK
+import arrow.core.None
 import arrow.core.Option
 import arrow.core.SequenceK
+import arrow.core.Some
 import arrow.core.extensions.ListKAlternative
+import arrow.core.fix
 import kotlin.Boolean
 import kotlin.Function0
 import kotlin.PublishedApi
@@ -20,6 +23,7 @@ import kotlin.jvm.JvmName
   "EXTENSION_SHADOWED_BY_MEMBER",
   "UNUSED_PARAMETER"
 )
+@Deprecated("This method results in StackOverflow")
 fun <A> List<A>.some(): List<SequenceK<A>> =
     arrow.core.extensions.list.alternative.List.alternative().run {
   arrow.core.ListK(this@some).some<A>() as kotlin.collections.List<arrow.core.SequenceK<A>>
@@ -32,10 +36,11 @@ fun <A> List<A>.some(): List<SequenceK<A>> =
   "EXTENSION_SHADOWED_BY_MEMBER",
   "UNUSED_PARAMETER"
 )
+@Deprecated("This method results in StackOverflow")
 fun <A> List<A>.many(): List<SequenceK<A>> =
-    arrow.core.extensions.list.alternative.List.alternative().run {
-  arrow.core.ListK(this@many).many<A>() as kotlin.collections.List<arrow.core.SequenceK<A>>
-}
+  arrow.core.extensions.list.alternative.List.alternative().run {
+    arrow.core.ListK(this@many).many<A>() as kotlin.collections.List<arrow.core.SequenceK<A>>
+  }
 
 @JvmName("alt")
 @Suppress(
@@ -44,10 +49,9 @@ fun <A> List<A>.many(): List<SequenceK<A>> =
   "EXTENSION_SHADOWED_BY_MEMBER",
   "UNUSED_PARAMETER"
 )
+@Deprecated("@extension kinded projected functions are deprecated", ReplaceWith("this + arg1"))
 infix fun <A> List<A>.alt(arg1: List<A>): List<A> =
-    arrow.core.extensions.list.alternative.List.alternative().run {
-  arrow.core.ListK(this@alt).alt<A>(arrow.core.ListK(arg1)) as kotlin.collections.List<A>
-}
+  this + arg1
 
 @JvmName("orElse")
 @Suppress(
@@ -56,10 +60,9 @@ infix fun <A> List<A>.alt(arg1: List<A>): List<A> =
   "EXTENSION_SHADOWED_BY_MEMBER",
   "UNUSED_PARAMETER"
 )
+@Deprecated("@extension kinded projected functions are deprecated", ReplaceWith("this + arg1"))
 fun <A> List<A>.orElse(arg1: List<A>): List<A> =
-    arrow.core.extensions.list.alternative.List.alternative().run {
-  arrow.core.ListK(this@orElse).orElse<A>(arrow.core.ListK(arg1)) as kotlin.collections.List<A>
-}
+  this + arg1
 
 @JvmName("combineK")
 @Suppress(
@@ -68,10 +71,9 @@ fun <A> List<A>.orElse(arg1: List<A>): List<A> =
   "EXTENSION_SHADOWED_BY_MEMBER",
   "UNUSED_PARAMETER"
 )
+@Deprecated("@extension kinded projected functions are deprecated", ReplaceWith("this + arg1"))
 fun <A> List<A>.combineK(arg1: List<A>): List<A> =
-    arrow.core.extensions.list.alternative.List.alternative().run {
-  arrow.core.ListK(this@combineK).combineK<A>(arrow.core.ListK(arg1)) as kotlin.collections.List<A>
-}
+  this + arg1
 
 @JvmName("optional")
 @Suppress(
@@ -80,10 +82,9 @@ fun <A> List<A>.combineK(arg1: List<A>): List<A> =
   "EXTENSION_SHADOWED_BY_MEMBER",
   "UNUSED_PARAMETER"
 )
+@Deprecated("@extension kinded projected functions are deprecated", ReplaceWith("map(::Some) + listOf(None)", "arrow.core.Some", "arrow.core.None"))
 fun <A> List<A>.optional(): List<Option<A>> =
-    arrow.core.extensions.list.alternative.List.alternative().run {
-  arrow.core.ListK(this@optional).optional<A>() as kotlin.collections.List<arrow.core.Option<A>>
-}
+  map(::Some) + listOf(None)
 
 @JvmName("guard")
 @Suppress(
@@ -92,9 +93,9 @@ fun <A> List<A>.optional(): List<Option<A>> =
   "EXTENSION_SHADOWED_BY_MEMBER",
   "UNUSED_PARAMETER"
 )
-fun guard(arg0: Boolean): List<Unit> = arrow.core.extensions.list.alternative.List
-   .alternative()
-   .guard(arg0) as kotlin.collections.List<kotlin.Unit>
+@Deprecated("@extension kinded projected functions are deprecated", ReplaceWith("if (arg0) listOf(Unit) else emptyList()"))
+fun guard(arg0: Boolean): List<Unit> =
+  if (arg0) listOf(Unit) else emptyList()
 
 @JvmName("lazyOrElse")
 @Suppress(
@@ -103,21 +104,22 @@ fun guard(arg0: Boolean): List<Unit> = arrow.core.extensions.list.alternative.Li
   "EXTENSION_SHADOWED_BY_MEMBER",
   "UNUSED_PARAMETER"
 )
+@Deprecated("@extension kinded projected functions are deprecated", ReplaceWith("this + arg1()"))
 fun <A> List<A>.lazyOrElse(arg1: Function0<Kind<ForListK, A>>): List<A> =
-    arrow.core.extensions.list.alternative.List.alternative().run {
-  arrow.core.ListK(this@lazyOrElse).lazyOrElse<A>(arg1) as kotlin.collections.List<A>
-}
+  this + arg1().fix()
 
 /**
  * cached extension
  */
 @PublishedApi()
 internal val alternative_singleton: ListKAlternative = object :
-    arrow.core.extensions.ListKAlternative {}
+  arrow.core.extensions.ListKAlternative {}
 
 object List {
   @Suppress(
     "UNCHECKED_CAST",
     "NOTHING_TO_INLINE"
   )
-  inline fun alternative(): ListKAlternative = alternative_singleton}
+  @Deprecated("Alternative typeclasses is deprecated. Use concrete methods on List")
+  inline fun alternative(): ListKAlternative = alternative_singleton
+}
