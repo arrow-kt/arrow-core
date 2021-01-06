f2ee69c7fde6b4f754ffb8bb4b23fdcb82871e8c
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-01-06 13:28:42 +0100

    Deprecate MapKSemialign.kt

--- arrow-core/src/main/kotlin/arrow/core/extensions/map/semialign/MapKSemialign.kt
+++ arrow-core/src/main/kotlin/arrow/core/extensions/map/semialign/MapKSemialign.kt
@@ -4,6 +4,7 @@ import arrow.core.Ior
 import arrow.core.Option
 import arrow.core.Tuple2
 import arrow.core.extensions.MapKSemialign
+import arrow.core.padZip
 import arrow.typeclasses.Semigroup
 import kotlin.Any
 import kotlin.Deprecated
@@ -24,15 +25,15 @@ import kotlin.jvm.JvmName
 @Deprecated(
   "@extension kinded projected functions are deprecated",
   ReplaceWith(
-  "align(arg0, arg1)",
-  "arrow.core.extensions.map.semialign.Map.align"
+    "arg0.align(arg1)",
+    "arrow.core.align"
   ),
   DeprecationLevel.WARNING
 )
 fun <K, A, B> align(arg0: Map<K, A>, arg1: Map<K, B>): Map<K, Ior<A, B>> =
-    arrow.core.extensions.map.semialign.Map
-   .semialign<K>()
-   .align<A, B>(arrow.core.MapK(arg0), arrow.core.MapK(arg1)) as kotlin.collections.Map<K,
+  arrow.core.extensions.map.semialign.Map
+    .semialign<K>()
+    .align<A, B>(arrow.core.MapK(arg0), arrow.core.MapK(arg1)) as kotlin.collections.Map<K,
     arrow.core.Ior<A, B>>
 
 @JvmName("alignWith")
@@ -45,8 +46,8 @@ fun <K, A, B> align(arg0: Map<K, A>, arg1: Map<K, B>): Map<K, Ior<A, B>> =
 @Deprecated(
   "@extension kinded projected functions are deprecated",
   ReplaceWith(
-  "alignWith(arg0, arg1, arg2)",
-  "arrow.core.extensions.map.semialign.Map.alignWith"
+    "arg0.align(arg1) { (_, ior) -> arg2(ior) }",
+    "arrow.core.align"
   ),
   DeprecationLevel.WARNING
 )
@@ -55,9 +56,9 @@ fun <K, A, B, C> alignWith(
   arg1: Map<K, B>,
   arg2: Function1<Ior<A, B>, C>
 ): Map<K, C> = arrow.core.extensions.map.semialign.Map
-   .semialign<K>()
-   .alignWith<A, B, C>(arrow.core.MapK(arg0), arrow.core.MapK(arg1), arg2) as
-    kotlin.collections.Map<K, C>
+  .semialign<K>()
+  .alignWith<A, B, C>(arrow.core.MapK(arg0), arrow.core.MapK(arg1), arg2) as
+  kotlin.collections.Map<K, C>
 
 @JvmName("salign")
 @Suppress(
@@ -69,16 +70,16 @@ fun <K, A, B, C> alignWith(
 @Deprecated(
   "@extension kinded projected functions are deprecated",
   ReplaceWith(
-  "salign(arg1, arg2)",
-  "arrow.core.salign"
+    "salign(arg1, arg2)",
+    "arrow.core.salign"
   ),
   DeprecationLevel.WARNING
 )
 fun <K, A> Map<K, A>.salign(arg1: Semigroup<A>, arg2: Map<K, A>): Map<K, A> =
-    arrow.core.extensions.map.semialign.Map.semialign<K>().run {
-  arrow.core.MapK(this@salign).salign<A>(arg1, arrow.core.MapK(arg2)) as kotlin.collections.Map<K,
-    A>
-}
+  arrow.core.extensions.map.semialign.Map.semialign<K>().run {
+    arrow.core.MapK(this@salign).salign<A>(arg1, arrow.core.MapK(arg2)) as kotlin.collections.Map<K,
+      A>
+  }
 
 @JvmName("padZip")
 @Suppress(
@@ -90,16 +91,15 @@ fun <K, A> Map<K, A>.salign(arg1: Semigroup<A>, arg2: Map<K, A>): Map<K, A> =
 @Deprecated(
   "@extension kinded projected functions are deprecated",
   ReplaceWith(
-  "padZip(arg1)",
-  "arrow.core.padZip"
+    "padZip(arg1) { _, a, b -> Tuple2(Option.fromNullable(a), Option.fromNullable(b)) }",
+    "arrow.core.padZip",
+    "arrow.core.Option",
+    "arrow.core.fromNullable"
   ),
   DeprecationLevel.WARNING
 )
 fun <K, A, B> Map<K, A>.padZip(arg1: Map<K, B>): Map<K, Tuple2<Option<A>, Option<B>>> =
-    arrow.core.extensions.map.semialign.Map.semialign<K>().run {
-  arrow.core.MapK(this@padZip).padZip<A, B>(arrow.core.MapK(arg1)) as kotlin.collections.Map<K,
-    arrow.core.Tuple2<arrow.core.Option<A>, arrow.core.Option<B>>>
-}
+  padZip(arg1) { _, a, b -> Tuple2(Option.fromNullable(a), Option.fromNullable(b)) }
 
 @JvmName("padZipWith")
 @Suppress(
@@ -111,16 +111,16 @@ fun <K, A, B> Map<K, A>.padZip(arg1: Map<K, B>): Map<K, Tuple2<Option<A>, Option
 @Deprecated(
   "@extension kinded projected functions are deprecated",
   ReplaceWith(
-  "padZipWith(arg1, arg2)",
-  "arrow.core.padZipWith"
+    "padZip(arg1) { _, a, b -> arg2(Option.fromNullable(a), Option.fromNullable(b)) }",
+    "arrow.core.padZip",
+    "arrow.core.Option",
+    "arrow.core.fromNullable"
   ),
   DeprecationLevel.WARNING
 )
 fun <K, A, B, C> Map<K, A>.padZipWith(arg1: Map<K, B>, arg2: Function2<Option<A>, Option<B>, C>):
-    Map<K, C> = arrow.core.extensions.map.semialign.Map.semialign<K>().run {
-  arrow.core.MapK(this@padZipWith).padZipWith<A, B, C>(arrow.core.MapK(arg1), arg2) as
-    kotlin.collections.Map<K, C>
-}
+  Map<K, C> =
+  padZip(arg1) { _, a, b -> arg2(Option.fromNullable(a), Option.fromNullable(b)) }
 
 /**
  * cached extension
@@ -133,5 +133,7 @@ object Map {
     "UNCHECKED_CAST",
     "NOTHING_TO_INLINE"
   )
+  @Deprecated("Functor typeclasses is deprecated. Use concrete methods on Map")
   inline fun <K> semialign(): MapKSemialign<K> = semialign_singleton as
-      arrow.core.extensions.MapKSemialign<K>}
+    arrow.core.extensions.MapKSemialign<K>
+}
