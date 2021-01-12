ccc6b7724d18441cbb12c38a97c4101f0b78cf47
raulraja <raulraja@gmail.com>
2021-01-12 13:45:25 +0100

    Remove Deprecated `Id<A>` data type.

--- arrow-core-data/src/main/kotlin/arrow/typeclasses/Traverse.kt
+++ arrow-core-data/src/main/kotlin/arrow/typeclasses/Traverse.kt
@@ -1,10 +1,7 @@
 package arrow.typeclasses
 
 import arrow.Kind
-import arrow.core.Id
 import arrow.core.identity
-import arrow.core.value
-import arrow.typeclasses.internal.IdBimonad
 import arrow.core.Option
 import arrow.core.Either
 import arrow.core.Validated
@@ -12,6 +9,9 @@ import arrow.core.Const
 import arrow.core.Nel
 import arrow.core.SequenceK
 import arrow.core.ValidatedNel
+import arrow.typeclasses.internal.Id
+import arrow.typeclasses.internal.fix
+import arrow.typeclasses.internal.idApplicative
 
 /**
  * ank_macro_hierarchy(arrow.typeclasses.Traverse)
@@ -529,7 +529,7 @@ interface Traverse<F> : Functor<F>, Foldable<F> {
   fun <G, A> Kind<F, Kind<G, A>>.sequence(AG: Applicative<G>): Kind<G, Kind<F, A>> = traverse(AG, ::identity)
 
   override fun <A, B> Kind<F, A>.map(f: (A) -> B): Kind<F, B> =
-    traverse(IdBimonad) { Id(f(it)) }.value()
+    traverse(idApplicative) { Id(f(it)) }.fix().value
 
   fun <G, A, B> Kind<F, A>.flatTraverse(MF: Monad<F>, AG: Applicative<G>, f: (A) -> Kind<G, Kind<F, B>>): Kind<G, Kind<F, B>> =
     AG.run { traverse(this, f).map { MF.run { it.flatten() } } }
