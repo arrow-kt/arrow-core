a966d3c1ff088b86c6400ccd5bb5dcbf0a8c7812
danimontoya <daniel.montoya@47deg.com>
2021-01-29 14:45:09 +0100

    Refactor Endo.combine

--- arrow-core-data/src/main/kotlin/arrow/core/Endo.kt
+++ arrow-core-data/src/main/kotlin/arrow/core/Endo.kt
@@ -6,11 +6,12 @@ import arrow.typeclasses.Monoid
  * The monoid of endomorphisms under composition.
  */
 data class Endo<A>(val f: (A) -> A) {
-  companion object
-}
 
-fun <A> Endo<A>.combine(g: Endo<A>): Endo<A> =
-  Endo(f.compose(g.f))
+  companion object
+
+  fun combine(g: Endo<A>): Endo<A> =
+    Endo(f.compose(g.f))
+}
 
 fun <A> Monoid.Companion.endo(): Monoid<Endo<A>> = object : Monoid<Endo<A>> {
   override fun empty(): Endo<A> =
