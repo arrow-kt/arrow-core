f905261266ccb29dadf9f5bc11c18aaf58dfe4fe
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-02-12 17:52:12 +0100

    Revert invoke usages to bind

--- arrow-core-data/src/test/kotlin/arrow/core/computations/NullableTest.kt
+++ arrow-core-data/src/test/kotlin/arrow/core/computations/NullableTest.kt
@@ -1,5 +1,6 @@
 package arrow.core.computations
 
+import arrow.core.Some
 import arrow.core.test.UnitSpec
 import io.kotlintest.shouldBe
 
@@ -18,10 +19,24 @@ class NullableTest : UnitSpec() {
         string
       } shouldBe "1"
     }
+    "binding option in nullable" {
+      nullable {
+        val number = Some("s".length)
+        val string = number.map(Int::toString).bind()
+        string
+      } shouldBe "1"
+    }
     "short circuit" {
       nullable {
         val number: Int = "s".length
         (number.takeIf { it > 1 }?.toString()).bind()
+        throw IllegalStateException("This should not be executed")
+      } shouldBe null
+    }
+    "short circuit option" {
+      nullable {
+        val number = Some("s".length)
+        number.filter { it > 1 }.map(Int::toString).bind()
         throw IllegalStateException("This should not be executed")
       } shouldBe null
     }
