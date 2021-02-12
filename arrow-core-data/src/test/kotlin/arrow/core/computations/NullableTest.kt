2214a61c0b40f1002d2d6c3db1a951108f30921a
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-02-12 17:23:36 +0100

    Replace invoke with bind in NullableTest

--- arrow-core-data/src/test/kotlin/arrow/core/computations/NullableTest.kt
+++ arrow-core-data/src/test/kotlin/arrow/core/computations/NullableTest.kt
@@ -8,52 +8,52 @@ class NullableTest : UnitSpec() {
   init {
     "simple case" {
       nullable {
-        "s".length().invoke()
+        "s".length.bind()
       } shouldBe 1
     }
     "multiple types" {
       nullable {
-        val number = "s".length()
-        val string = number.toString()()
+        val number = "s".length
+        val string = number.toString().bind()
         string
       } shouldBe "1"
     }
     "short circuit" {
       nullable {
-        val number: Int = "s".length()
-        (number.takeIf { it > 1 }?.toString())()
+        val number: Int = "s".length
+        (number.takeIf { it > 1 }?.toString()).bind()
         throw IllegalStateException("This should not be executed")
       } shouldBe null
     }
     "when expression" {
       nullable {
-        val number = "s".length()
+        val number = "s".length.bind()
         val string = when (number) {
           1 -> number.toString()
           else -> null
-        }.invoke()
+        }.bind()
         string
       } shouldBe "1"
     }
     "if expression" {
       nullable {
-        val number = "s".length()
+        val number = "s".length.bind()
         val string = if (number == 1) {
           number.toString()
         } else {
           null
-        }.invoke()
+        }.bind()
         string
       } shouldBe "1"
     }
     "if expression short circuit" {
       nullable {
-        val number = "s".length()
+        val number = "s".length.bind()
         val string = if (number != 1) {
           number.toString()
         } else {
           null
-        }()
+        }.bind()
         string
       } shouldBe null
     }
